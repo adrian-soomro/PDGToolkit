a85f985363887b32dcfa717037f14157f7945b9c
Adrian Soomro <adrian.soomro@gmail.com>
2020-09-12 18:07:25 +0100

    Update canvas to follow the Toolkit

---
+++ src/PDGCanvas/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    relativePathToDungeon: '../../dungeon.json',
+  };
--- src/PDGCanvas/routes/index.js
+++ src/PDGCanvas/routes/index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
 const path = require('path');
+const config = require('../config');
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 router.get('/data', function(req, res, next) {
-  res.sendFile(path.join(__dirname+'/data.json'));
+  res.sendFile(path.resolve(config.relativePathToDungeon));
 });
+
 
 module.exports = router;
