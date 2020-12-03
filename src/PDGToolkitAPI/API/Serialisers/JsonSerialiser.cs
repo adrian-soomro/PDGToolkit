e6428fa7dfbac28cf56bbfda98af96dd87d9272e
Adrian Soomro <37105695+assoomro@users.noreply.github.com>
2020-12-03 20:02:53 +0000

    Refactor application layout (#13)

--- src/PDGToolkitAPI/API/Serialisers/JsonSerialiser.cs
+++ src/PDGToolkitAPI/API/Serialisers/JsonSerialiser.cs
@@ -1,7 +1,7 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
-namespace PDGToolkitAPI.Application.Serialisers
+namespace PDGToolkitAPI.API.Serialisers
 {
     public class JsonSerialiser : ISerialiser
     {
