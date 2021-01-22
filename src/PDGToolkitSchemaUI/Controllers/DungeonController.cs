61c866719beecfeef7b0384ddf3d3e63707c338f
Adrian Soomro <adrian.soomro@gmail.com>
2021-01-22 12:19:34 +0000

    Update Schema UI component

--- src/PDGToolkitSchemaUI/Controllers/DungeonController.cs
+++ src/PDGToolkitSchemaUI/Controllers/DungeonController.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PDGToolkitCore.Domain.Models;
 
-namespace PDGToolkitUI.Controllers
+namespace PDGToolkitSchemaUI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- src/PDGToolkitSchemaUI/Startup.cs
+++ src/PDGToolkitSchemaUI/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Hosting;
 
 using Microsoft.OpenApi.Models;
 
-namespace PDGToolkitUI
+namespace PDGToolkitSchemaUI
 {
     public class Startup
     {
