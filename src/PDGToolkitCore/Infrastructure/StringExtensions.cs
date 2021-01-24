63d9466af4790951e13a95fc3f3229964c84a2b0
assoomro <adrian.soomro@gmail.com>
2021-01-24 16:27:01 +0000

    Make regex more robust for cases
    when the project is cloned into a directory with the same name

--- src/PDGToolkitCore/Infrastructure/StringExtensions.cs
+++ src/PDGToolkitCore/Infrastructure/StringExtensions.cs
@@ -11,7 +11,7 @@ namespace PDGToolkitCore.Infrastructure
         public static string ToAbsolutePathFromPathRelativeToSolutionRoot(this string relativeFilePath)
         {
             var exePath = Path.GetDirectoryName(Directory.GetCurrentDirectory());
-            Regex appPathMatcher = new Regex(@"^(.*?)PDGToolkit");
+            Regex appPathMatcher = new Regex(@"^(.*?)(PDGToolkit(\\|\/))+");
             var appRoot = appPathMatcher.Match(exePath).Value;
             return Path.Combine(appRoot, relativeFilePath);
         }
