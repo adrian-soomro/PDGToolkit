c5d19d9781bf87111a3744b7615d316af010bdc9
assoomro <adrian.soomro@gmail.com>
2021-02-05 10:44:19 +0000

    Add width and height options

--- src/PDGToolkitCLI/Program.cs
+++ src/PDGToolkitCLI/Program.cs
@@ -21,6 +21,14 @@ namespace PDGToolkitCLI
         [Option("-p|--path", Description = @"Relative path to where the output file should be stored, 
                                                       including the file's name. Relative to the solution root.")]
         public string PathToOutputFile { get; } = "dungeon.json";
+        
+        [Option("-w|--width", Description = @"Sets the width of the generated dungeon.")]
+        [Range(1, int.MaxValue)]
+        public int DungeonWidth { get; } = 1280;
+        
+        [Option("--height", Description = @"Sets the height of the generated dungeon.")]
+        [Range(1, int.MaxValue)]
+        public int DungeonHeight { get; } = 720;
 
         [Option("-l|--list", Description = @"Lists all generators in the toolkit.")]
         public bool ListGenerators { get; }
@@ -35,6 +43,8 @@ namespace PDGToolkitCLI
             }
             
             SettingsHandler.EditOutputRelativePathSetting(PathToOutputFile);
+            SettingsHandler.EditHeightSetting(DungeonHeight);
+            SettingsHandler.EditWidthSetting(DungeonWidth);
             var runner = Startup.InitialiseRunner(Generator);
             await runner.Run();
         }
--- src/PDGToolkitCLI/SettingsHandler.cs
+++ src/PDGToolkitCLI/SettingsHandler.cs
@@ -7,16 +7,42 @@ namespace PDGToolkitCLI
      * by modifying certain fields of hte config file.
      * This results in modifying certain settings of the toolkit that apply to only one run.
      */
-    public class SettingsHandler
+    public static class SettingsHandler
     {
+        private static readonly string PathToConfigInOutDirectory =
+            Path.Combine(Directory.GetCurrentDirectory(), "config.json");
+        
         internal static void EditOutputRelativePathSetting(string relativePath)
+        {
+            var settings = ReadSettingsObject();
+            settings["outputRelativePath"] = relativePath;
+            string output = Newtonsoft.Json.JsonConvert.SerializeObject(settings, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText(PathToConfigInOutDirectory, output);
+        }
+
+        internal static void EditHeightSetting(int height)
+        {
+            var settings = ReadSettingsObject();
+            settings["grid"]["height"] = height.ToString();
+            string output = Newtonsoft.Json.JsonConvert.SerializeObject(settings, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText(PathToConfigInOutDirectory, output);
+        }
+        
+        internal static void EditWidthSetting(int width)
+        {
+            var settings = ReadSettingsObject();
+            settings["grid"]["width"] = width.ToString();
+            string output = Newtonsoft.Json.JsonConvert.SerializeObject(settings, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText(PathToConfigInOutDirectory, output);
+        }
+
+        private static dynamic ReadSettingsObject()
         {
             var pathToConfigInOutDirectory = Path.Combine(Directory.GetCurrentDirectory(), "config.json");
             var jsonAsText = File.ReadAllText(pathToConfigInOutDirectory);
-            dynamic settings = Newtonsoft.Json.JsonConvert.DeserializeObject(jsonAsText);
-            settings["outputRelativePath"] = relativePath;
-            string output = Newtonsoft.Json.JsonConvert.SerializeObject(settings, Newtonsoft.Json.Formatting.Indented);
-            File.WriteAllText(pathToConfigInOutDirectory, output);
+            return Newtonsoft.Json.JsonConvert.DeserializeObject(jsonAsText);
         }
+        
+        
     }
 }
