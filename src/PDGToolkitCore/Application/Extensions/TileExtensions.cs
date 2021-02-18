99ae8cf1596b202a5022eb3fed77c23195278365
assoomro <adrian.soomro@gmail.com>
2021-02-18 16:27:14 +0000

    Add extension for replacing tiles

---
+++ src/PDGToolkitCore/Application/Extensions/TileExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using PDGToolkitCore.Domain.Models;
+
+namespace PDGToolkitCore.Application.Extensions
+{
+    public static class TileExtensions
+    {
+        /**
+         * Given a list of tiles, and a replacement tile,
+         * Finds all tiles that share position with the replacement, removes them and
+         * Adds the replacement to the list of tiles.
+         */
+        public static void ReplaceTilesWithOtherTile(this List<Tile> tiles, Tile replacement)
+        {
+            var allTilesAtThisPosition = tiles.FindAll(t => t.Position.Equals(replacement.Position));
+
+            foreach (var tile in allTilesAtThisPosition)
+            {
+                tiles.Remove(tile);
+            }
+            
+            tiles.Add(replacement);
+        }
+    }
+}
---
+++ tests/PDGToolkitCore.UnitTests/TileExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using PDGToolkitCore.Application.Extensions;
+using PDGToolkitCore.Domain.Models;
+
+namespace PDGToolkitCore.UnitTests
+{
+    [TestFixture]
+    public class TileExtensionsTests
+    {
+        private readonly List<Tile> allTiles = new List<Tile>();
+        private readonly Position sharedPosition = new Position(0,0);
+        
+        [SetUp]
+        public void Init()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                allTiles.Add(new Tile(TileType.Wall, sharedPosition));
+            }
+        }
+
+        [Test]
+        public void Extension_ReplacesTilesWithSharedPosition()
+        {
+            var replacement = new Tile(TileType.Floor, sharedPosition);
+            
+            allTiles.ReplaceTilesWithOtherTile(replacement);
+            allTiles.Count().Should().Be(1);
+            allTiles.First().Should().BeEquivalentTo(replacement);
+        }
+        
+    }
+}
