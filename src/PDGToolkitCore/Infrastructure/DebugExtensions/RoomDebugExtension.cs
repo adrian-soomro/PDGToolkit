d8477e8e1ad50eb801eaa96abd8a0fc933745737
assoomro <adrian.soomro@gmail.com>
2021-02-18 11:24:48 +0000

    Add debug extension method

---
+++ src/PDGToolkitCore/Infrastructure/DebugExtensions/RoomDebugExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PDGToolkitCore.Domain.Models;
+
+namespace PDGToolkitCore.Infrastructure.DebugExtensions
+{
+    public static class RoomDebugExtension
+    {
+        public static void ListRooms(this List<Room> rooms)
+        {
+            for (int i = 0; i < rooms.Count; i++)
+            {
+                var tile = rooms[i].Tiles.First();
+                Console.Out.WriteLine($"Room #{i+1} => Id: {rooms[i].Id}, around: {tile.Position.X}, {tile.Position.Y}");
+            }
+        }
+    }
+}
