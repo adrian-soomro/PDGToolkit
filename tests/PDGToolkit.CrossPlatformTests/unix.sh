1b7cfe8968ba045ea2eb9b271a75299115381fb0
assoomro <adrian.soomro@gmail.com>
2021-04-30 10:55:21 +0100

    Get more info

--- tests/PDGToolkit.CrossPlatformTests/unix.sh
+++ tests/PDGToolkit.CrossPlatformTests/unix.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
 # go to the root of the repo
-#cd ../../
-ls
+
 
 # build the toolkit
 dotnet publish ./src/PDGToolkitCLI/PDGToolkitCLI.csproj -c Debug --output ./out
 
 cd ./out
 
+ls
+
+./PDGToolkitCLI -h
+echo "####"
 # run the toolkit
 ./PDGToolkitCLI
 
+echo "[ ] should've finished the execution, taking a nap."
+sleep 30
+
 ls
+
+echo "####"
 
 ls ../
 
+echo "###"
+
+cat ../dungeon.json
 # if dungeon.json exists, spin up canvas, load the page, fetch the image and persist it.
