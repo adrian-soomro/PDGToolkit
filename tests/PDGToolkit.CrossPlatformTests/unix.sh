9d10050a9c7d56a02f3f2a8ce7c59524be908a58
assoomro <adrian.soomro@gmail.com>
2021-05-01 11:06:47 +0100

    Tidy up

--- tests/PDGToolkit.CrossPlatformTests/unix.sh
+++ tests/PDGToolkit.CrossPlatformTests/unix.sh
@@ -1,35 +1,22 @@
 #!/bin/bash
 
 rootDir=$PWD
-
 # build the toolkit
 dotnet publish ./src/PDGToolkitCLI/PDGToolkitCLI.csproj -c Debug --output ./out
-
 cd ./out
-
-ls
-
-dotnet PDGToolkitCLI.dll -h
-echo "####"
-
 # run the toolkit
 dotnet PDGToolkitCLI.dll
-
 cd "$rootDir"
 mv ../dungeon.json ./dungeon.json
-
-
+# start the canvas in the background
 cd ./src/PDGCanvas/
 npm run start &
 canvasPID=$!
 echo "Starting up canvas ($canvasPID), giving it a minute to start up"
 sleep 60
 cd "$rootDir"
-
+# fetch OS, to distinquish between Darwin(MacOS) and Linux results
 os=$(uname -s)
 bash ./tests/PDGToolkit.CrossPlatformTests/saveDungeon.sh $os
-
-ls $rootDir
-
 # cleanup
 kill $canvasPID
--- tests/PDGToolkit.CrossPlatformTests/windows.ps1
+++ tests/PDGToolkit.CrossPlatformTests/windows.ps1
@@ -1,29 +1,18 @@
 $rootDir=Get-Location
-
 # build the toolkit
 dotnet publish ./src/PDGToolkitCLI/PDGToolkitCLI.csproj -c Debug --output ./out
-
 cd ./out
-
-ls
-
 # run the toolkit
 dotnet PDGToolkitCLI.dll
-
 cd "$rootDir"
 mv ../dungeon.json ./dungeon.json
-
-
+# start the canvas in the background
 cd ./src/PDGCanvas/
 $canvas = Start-Job -ScriptBlock {npm run start }
 Write-Host "Starting up canvas... giving it a minute to start up"
 Start-Sleep -s 60
 cd "$rootDir"
-
-### Save 
+# save the dungeon
 ./tests/PDGToolkit.CrossPlatformTests/saveDungeon.ps1
-
-ls $rootDir
-
 # cleanup
 Remove-Job -Job $canvas -Force
